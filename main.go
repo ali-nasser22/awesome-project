f0e62bfc5ad603a4aadacfc078c218962021acc5
Ali Nasser <alinasser.t22@gmail.com>
2025-08-16 05:49:46 +0300

    adding gin framework

--- main.go
+++ main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+
+	r.GET("/events", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	err := r.Run(":8080") // localhost:8080
+	if err != nil {
+		return
+	}
+}
