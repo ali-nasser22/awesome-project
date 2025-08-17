793e8f3693346ab205e12acd14e523d6ebba2736
Ali Nasser <alinasser.t22@gmail.com>
2025-08-17 11:37:54 +0300

    adding sqlite connection

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"awesomeProject/db"
+	"awesomeProject/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -8,11 +10,39 @@ import (
 
 func main() {
 
+	db.InitDB()
+
 	r := gin.Default()
 
 	r.GET("/events", func(c *gin.Context) {
+		events, err := models.GetAllEvents()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+		}
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello World",
+			"events": events,
+		})
+	})
+
+	r.POST("/events", func(c *gin.Context) {
+		var event models.Event
+		err := c.ShouldBindJSON(&event)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		createdStatus, err := event.Save()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+		}
+		c.JSON(createdStatus, gin.H{
+			"message": "event created successfully",
 		})
 	})
 
--- models/event.go
+++ models/event.go
@@ -1,23 +1,62 @@
 package models
 
 import (
+	"awesomeProject/db"
 	"net/http"
 	"time"
 )
 
 type Event struct {
-	ID          int       `json:"id"`
-	Name        string    `json:"name"`
-	Description string    `json:"description"`
-	Location    string    `json:"location"`
+	ID          int64     `json:"id"`
+	Name        string    `json:"name" binding:"required"`
+	Description string    `json:"description" binding:"required"`
+	Location    string    `json:"location" binding:"required"`
 	DateTime    time.Time `json:"dateTime"`
-	UserID      int       `json:"userID"`
+	UserID      string    `json:"userID" binding:"required"`
 }
 
 var events []Event
 
 func (e Event) Save() (int, error) {
-	// TODO: save event to database
-	events = append(events, e)
+	query := "INSERT INTO events (name,description,dateTime, location,userId) VALUES (?, ?,?,?,?)"
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+	defer stmt.Close()
+	e.DateTime = time.Now()
+	result, err := stmt.Exec(e.Name, e.Description, e.DateTime, e.Location, e.UserID)
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+	id, err := result.LastInsertId()
+	e.ID = id
 	return http.StatusCreated, nil
+}
+
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(
+			&event.ID,
+			&event.Name,
+			&event.Description,
+			&event.Location,
+			&event.UserID,
+			&event.DateTime,
+		)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+
+	return events, nil
+
 }
